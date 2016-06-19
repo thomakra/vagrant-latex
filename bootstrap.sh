097ec5f930a4c6cb24758a6fdb041771fff67888
Thomas Kræmer <thomas.kramer@uit.no>
2016-06-19 21:19:31 +0200

    add comment showing install of TeXLive using installer rather than apt-get

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,3 +2,9 @@
 
 apt-get update
 apt-get install -y texlive-full biber xindy latexmk asymptote pdftk
+#wget http://mirror.ctan.org/systems/texlive/tlnet/install-tl-unx.tar.gz
+#tar zxf install-tl-unx.tar.gz
+#cd install-tl-20160616
+#sudo ./install-tl
+# Press I <enter>
+# echo "export PATH=/usr/local/texlive/2016/bin/x86_64-linux:$PATH" >> ~/.bashrc
